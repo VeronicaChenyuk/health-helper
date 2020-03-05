4a6af343fa107daebdd1b0b6dc53dc72466dbd31
MishaVyuzh <vyuzzzh@yandex.ru>
2020-03-05 10:58:23 +0300

    create start skeleton

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Header from '../Header/Header.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
