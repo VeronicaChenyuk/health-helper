ed9ed9ed9d9da2d440e5ea71bac4f481fe0b372a
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-03-09 14:41:31 +0300

    Doctor page save to back

--- frontend/src/redux/action-types.js
+++ frontend/src/redux/action-types.js
@@ -2,6 +2,7 @@ const LOG_IN = 'LOG_IN';
 const LOG_OUT = 'LOG_OUT';
 const IS_LOGIN_FORM = 'IS_LOGIN_FORM';
 
+
 module.exports = {
   LOG_IN,
   LOG_OUT,
--- frontend/src/redux/root-reducer.js
+++ frontend/src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import loginReducer from './login-reducer';
 import switchFormReducer from './switch-form-reducer';
 
+
 // Root reducer
 export default combineReducers({
   logIn: loginReducer,
