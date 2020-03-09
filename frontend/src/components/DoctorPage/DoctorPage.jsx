6e7a3206cbbc805fca1745ee150fcfb65aab8d52
MishaVyuzh <vyuzzzh@yandex.ru>
2020-03-09 17:44:52 +0300

    refactor(): changeInfoUser fix import

--- frontend/src/components/DoctorPage/DoctorPage.jsx
+++ frontend/src/components/DoctorPage/DoctorPage.jsx
@@ -3,7 +3,8 @@ import {
   Button, Form, FormGroup, Label, Input, FormText,
 } from 'reactstrap';
 import { connect } from 'react-redux';
-import changeInfoUser from '../../redux/actions';
+import { withRouter } from 'react-router-dom';
+import { changeInfoUser } from '../../redux/actions';
 
 const loadHandler = async (e, props) => {
   console.log(props, 'WTFFFF');
@@ -12,7 +13,7 @@ const loadHandler = async (e, props) => {
   const doctorName = e.target.doctorName.value;
   const specialist = e.target.specialist.value;
   const { email } = props.user;
-  const changeInfo = props.changeInfoUser;
+  const { changeInfo } = props;
   const response = await fetch(e.target.action, {
     method: 'POST',
     headers:
@@ -32,35 +33,39 @@ const loadHandler = async (e, props) => {
   return true;
 };
 
-const DoctorPage = (props) => (
-  <div>
-    <Form method="POST" action="http://localhost:5000/personal/doctor" onSubmit={(e) => loadHandler(e, props)}>
-      <FormGroup>
-        <Label for="doctorName">Имя Отчество Фамилия</Label>
-        <Input type="doctorName" name="doctorName" id="doctorName" placeholder="{doctorName}" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="specialist">Специальность</Label>
-        <Input type="specialist" name="specialist" id="specialist" placeholder="{specialistM}" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">Файл</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted" />
-      </FormGroup>
-      <Button type="submit">Изменить</Button>
-    </Form>
-  </div>
-);
+const DoctorPage = (props) => {
+  const { doctorName, specialist } = props.user;
+
+  return (
+    <div>
+      <Form method="POST" action="http://localhost:5000/personal/doctor" onSubmit={(e) => loadHandler(e, props)}>
+        <FormGroup>
+          <Label for="doctorName">Имя Отчество Фамилия</Label>
+          <Input type="doctorName" name="doctorName" id="doctorName" placeholder="Введите Имя, Фамилию и Отчество" defaultValue={doctorName} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="specialist">Специальность</Label>
+          <Input type="specialist" name="specialist" id="specialist" placeholder="Введите специальность" defaultValue={specialist}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleFile">Файл</Label>
+          <Input type="file" name="file" id="exampleFile" />
+          <FormText color="muted" />
+        </FormGroup>
+        <Button type="submit">Изменить</Button>
+      </Form>
+    </div>
+  );
+};
 const mapStateToProps = (state) => ({
   user: state.logIn.user,
 });
 
 const mapDispathToProps = (dispatch) => ({
-  changeInfoUser: (user) => dispatch(changeInfoUser(user)),
+  changeInfo: (user) => dispatch(changeInfoUser(user)),
 });
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispathToProps,
-)(DoctorPage);
+)(DoctorPage));
