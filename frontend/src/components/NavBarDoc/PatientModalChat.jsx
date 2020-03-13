30b77224b78d6b1c9f6832a6b60bc41fd9b444dc
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-03-13 12:50:11 +0300

    Send message

--- frontend/src/components/NavBarDoc/PatientModalChat.jsx
+++ frontend/src/components/NavBarDoc/PatientModalChat.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import {
-  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Input
 } from 'reactstrap';
-
+import './PatientModalChat.css'
 const PatientModalReport = (props) => {
   const {
     buttonLabel,
@@ -13,19 +13,45 @@ const PatientModalReport = (props) => {
 
   const toggle = () => setModal(!modal);
 
+  const saveMessage = async (event) => {
+    event.preventDefault();
+    const action = event.target.action;
+    const textDoctor = event.target.textDoctor.value;
+    const email = event.target.email.value;
+    const response = await fetch(action, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        textDoctor,
+        email,
+      })
+    })
+    const result = await response.json();
+
+
+  }
+
   return (
     <div>
       <Button onClick={toggle}>Cвязаться с пациентом</Button>
-      <Modal isOpen={modal} toggle={toggle} className={className} style={{ minWidth: '1200px', minHeight: '800px' }}>
-        <ModalHeader toggle={toggle}>Письмо Иванову Ивану</ModalHeader>
-        <ModalBody>
-          Здесь мы будем писать сообщение пациенту
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>SendLetter</Button>
-          {' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
+      <Modal isOpen={modal} toggle={toggle} className={className} style={{ minWidth: '800px', minHeight: '800px' }}>
+        <ModalHeader toggle={toggle}>Ваше сообщение</ModalHeader>
+        <form method="POST" action='http://localhost:5000/savemessage' >
+          <ModalBody>
+            <p className ='littleinput'>
+              <Input name='email' type='text' defaultValue='Введите почту'></Input>
+            </p>
+            
+            <textarea name="textDoctor" cols="72" rows="4" ></textarea>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" type='submit' onClick={()=>saveMessage()}>Отправить письмо</Button>
+            {' '}
+            <Button color="secondary" >Выход</Button>
+          </ModalFooter>
+        </form>
       </Modal>
     </div>
   );
